07c15432293bb9f17cdc386e941a65e3117c02c9
Anais Hebert <anaishebert@Anaiss-MacBook-Pro.local>
2019-08-05 14:36:56 -0700

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -3,15 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense, editExpense, removeExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
 
-
+console.log('test');
+console.log('test2');
 const jsx = (
     <Provider store={store}>
         <AppRouter />
